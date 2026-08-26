a698b963a3e758f597e6e28d7be52963b14c59dd
BIGDUKE004 <elijahmiracle2007@gmail.com>
2026-08-26 16:08:11 +0100

    updated query

--- Sql/Chapter2/chapter_two_exercise.sql
+++ Sql/Chapter2/chapter_two_exercise.sql
@@ -1,3 +1,5 @@
+USE cape_codd;
+
 SELECT SKU, SKU_Description
 FROM INVENTORY
 
@@ -75,6 +77,9 @@ FROM INVENTORY;
 
 -- COUNT count all the values in the column while sum takes all the value in the particular column and sum it up-- 
 
+
+
+
 SELECT WarehouseID, SUM(QuantityOnHand) AS TotalItemsOnHand
 FROM INVENTORY
 GROUP BY WarehouseID
@@ -97,4 +102,72 @@ ORDER BY TotalItemsOnHandLT3 DESC
 
 -- the where clause was applied first why is because we need to check for the sum first because it came first then we check for the sku because we need to have the sum first before we filter out the sku--  
 
+
+SELECT INVENTORY.SKU, INVENTORY.SKU_Description, WAREHOUSE.WarehouseID, WAREHOUSE.WarehouseCity, WAREHOUSE.WarehouseState 
+FROM INVENTORY
+JOIN WAREHOUSE 
+ON INVENTORY.WarehouseID = WAREHOUSE.WarehouseID 
+WHERE WAREHOUSE.WarehouseCity = 'Atlanta' 
+OR WAREHOUSE.WarehouseCity = 'Bangor' 
+OR WAREHOUSE.WarehouseCity = 'Chicago';
+
+SELECT INVENTORY.SKU, INVENTORY.SKU_Description, WAREHOUSE.WarehouseID, WAREHOUSE.WarehouseCity, WAREHOUSE.WarehouseState 
+FROM INVENTORY
+JOIN WAREHOUSE 
+ON INVENTORY.WarehouseID = WAREHOUSE.WarehouseID 
+WHERE WAREHOUSE.WarehouseCity 
+IN ('Atlanta', 'Bangor', 'Chicago');
+
+SELECT INVENTORY.SKU, INVENTORY.SKU_Description, WAREHOUSE.WarehouseID, WAREHOUSE.WarehouseCity, WAREHOUSE.WarehouseState 
+FROM INVENTORY
+JOIN WAREHOUSE 
+ON INVENTORY.WarehouseID = WAREHOUSE.WarehouseID 
+WHERE WAREHOUSE.WarehouseCity <> 'Atlanta' 
+AND WAREHOUSE.WarehouseCity <> 'Bangor' AND WAREHOUSE.WarehouseCity <> 'Chicago';
+
+SELECT INVENTORY.SKU, INVENTORY.SKU_Description, WAREHOUSE.WarehouseID, WAREHOUSE.WarehouseCity, WAREHOUSE.WarehouseState 
+FROM INVENTORY
+JOIN WAREHOUSE 
+ON INVENTORY.WarehouseID = WAREHOUSE.WarehouseID 
+WHERE WAREHOUSE.WarehouseCity NOT IN ('Atlanta', 'Bangor', 'Chicago');
+
+SELECT CONCAT(SKU_Description, ' is located in ', WarehouseCity) 
+AS ItemLocation 
+FROM INVENTORY 
+JOIN WAREHOUSE 
+ON INVENTORY.WarehouseID = WAREHOUSE.WarehouseID;
+
+SELECT SKU, SKU_Description, WarehouseID 
+FROM INVENTORY 
+WHERE WarehouseID 
+IN (SELECT WarehouseID FROM WAREHOUSE WHERE Manager = 'Lucille Smith');
+
+SELECT INVENTORY.SKU, INVENTORY.SKU_Description, INVENTORY.WarehouseID 
+FROM INVENTORY, WAREHOUSE 
+WHERE INVENTORY.WarehouseID = WAREHOUSE.WarehouseID 
+AND WAREHOUSE.Manager = 'Lucille Smith';
+
+SELECT INVENTORY.SKU, INVENTORY.SKU_Description, INVENTORY.WarehouseID 
+FROM INVENTORY 
+JOIN WAREHOUSE 
+ON INVENTORY.WarehouseID = WAREHOUSE.WarehouseID 
+WHERE WAREHOUSE.Manager = 'Lucille Smith';
+
+SELECT WarehouseID, AVG(QuantityOnHand) AS AverageQuantityOnHand 
+FROM INVENTORY 
+WHERE WarehouseID 
+IN (SELECT WarehouseID FROM WAREHOUSE WHERE Manager = 'Lucille Smith') 
+GROUP BY WarehouseID;
+
+SELECT INVENTORY.WarehouseID, AVG(INVENTORY.QuantityOnHand) AS AverageQuantityOnHand 
+FROM INVENTORY, WAREHOUSE
+WHERE INVENTORY.WarehouseID = WAREHOUSE.WarehouseID AND WAREHOUSE.Manager = 'Lucille Smith' 
+GROUP BY INVENTORY.WarehouseID;
+
+SELECT INVENTORY.WarehouseID, AVG(INVENTORY.QuantityOnHand) AS AverageQuantityOnHand 
+FROM INVENTORY
+JOIN WAREHOUSE 
+ON INVENTORY.WarehouseID = WAREHOUSE.WarehouseID 
+WHERE WAREHOUSE.Manager = 'Lucille Smith' 
+GROUP BY INVENTORY.WarehouseID;
  
